8d2a8090a7b3a701a3957668866708572d647e9f
Intel <jimmdigriz@yandex.ru>
2015-09-22 22:40:49 +0300

    Game class

--- Hangman/Game.cs
+++ Hangman/Game.cs
@@ -3,23 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace Hangman
 {
     class Game
     {
         private string Word { get; set; }
+        public int Lenght { get; private set; }
         private int Stage { get; set; }
+        public char[] Alphabet { get; private set; }
 
-        public Game(string word)
+        public Game(string word, GameLanguage lang)
         {
             Word = word;
+            Lenght = Word.Length;
             Stage = 0;
+            InitializeAlphabet(lang);
+        }
+
+        private void InitializeAlphabet(GameLanguage lang)
+        {
+            switch (lang)
+            {
+                case GameLanguage.En:
+                    Alphabet = new char[] {'A', 'B', 'C', 'D', 'E',
+                        'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+                        'X', 'Y', 'Z'};
+                    break;
+                case GameLanguage.Ru:
+                    Alphabet = new char[] {'А', 'Б', 'В', 'Г', 'Д', 'Е',
+                        'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О',
+                        'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш',
+                        'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
+                    break;
+            }
+        }
+
+        public BitmapImage GetStageImage()
+        {
+            return new BitmapImage(
+                new Uri(System.IO.Path.Combine(
+                    Environment.CurrentDirectory,
+                    "Images", Stage+".png")));
         }
 
         public int[] TakeCharacter(char ch)
         {
-            return null;
+            int[] temp = new int[Word.Length];
+
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] == ch)
+                {
+                    temp[i] = 1;
+                }
+                else
+                {
+                    temp[i] = 0;
+                }
+            }
+
+            if (temp.Count(i => i == 1) == 0)
+            {
+                Stage++;
+            }
+
+            return temp;
         }
     }
 }
--- Hangman/MainWindow.xaml.cs
+++ Hangman/MainWindow.xaml.cs
@@ -12,47 +12,126 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
 
 namespace Hangman
 {
+    public enum GameLanguage
+    {
+        En,
+        Ru
+    }
+
     public partial class MainWindow : Window
     {
-        private enum Language
-        { 
-            En,
-            Ru
-        }
+        private Game HangmanGame { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            CreateCharacterLbl(0);
-            CreateCharacterLbl(1);
+            InitializeGameField();
         }
 
-        private void CreateCharacterLbl(int i)
+        private void NewGameBtnClick(object sender, RoutedEventArgs e)
         {
-            Label label = new Label();
-            label.FontSize = 20;
-            label.FontWeight = FontWeight;
-            label.HorizontalContentAlignment = HorizontalAlignment.Center;
-            label.VerticalContentAlignment = VerticalAlignment.Center;
-            label.BorderThickness = new Thickness(1, 1, 1, 1);
-            label.BorderBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x2D, 0x2D, 0x30));
-            label.Height = label.Width = 38;
-            label.HorizontalAlignment = HorizontalAlignment.Left;
-            label.VerticalAlignment = VerticalAlignment.Top;
-
-            label.Name = "Character" + i.ToString();
-
-            label.Margin = new Thickness(i * label.Width, 0d, 0d, 0d);
-
-            GameGrid.Children.Add(label);
+            InitializeGameField();
         }
 
-        private void CreateCharacterBtns()
-        { 
-            
+        private void CharacterBtnClick(object sender, RoutedEventArgs e)
+        {
+            int[] temp = HangmanGame.TakeCharacter((char)((Button)sender).Content);
+
+            (sender as Button).IsEnabled = false;
         }
+
+        private void InitializeGameField()
+        {
+            HangmanGame = new Game("test", GameLanguage.En);
+
+            CreateImage();
+            CreateNewGameBtn();
+            CreateCharacterBtns(HangmanGame.Alphabet);
+            CreateCharacterLbl(HangmanGame.Lenght);
+        }
+
+        #region Game Field Initialization
+        private void CreateNewGameBtn()
+        {
+            Button button = new Button();
+            button.VerticalAlignment = VerticalAlignment.Center;
+            button.HorizontalAlignment = HorizontalAlignment.Right;
+
+            button.Name = "NewGameBtn";
+            button.Click += new RoutedEventHandler(NewGameBtnClick);
+
+            GameGrid.Children.Add(button);
+        }
+
+        private void CreateImage()
+        {
+            Image image = new Image();
+
+            image.Name = "StageImage";
+            image.VerticalAlignment = VerticalAlignment.Center;
+            image.HorizontalAlignment = HorizontalAlignment.Center;
+            image.Width = 150;
+            image.Height = 150;
+
+            GameGrid.Children.Add(image);
+        }
+
+        private void CreateCharacterLbl(int lenght)
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                Label label = new Label();
+                label.FontSize = 20;
+                label.FontWeight = FontWeight;
+                label.HorizontalContentAlignment = HorizontalAlignment.Center;
+                label.VerticalContentAlignment = VerticalAlignment.Center;
+                label.BorderThickness = new Thickness(1, 1, 1, 1);
+                label.BorderBrush = new SolidColorBrush(Color.FromArgb(0xFF, 0x2D, 0x2D, 0x30));
+                label.Height = label.Width = 38;
+                label.HorizontalAlignment = HorizontalAlignment.Left;
+                label.VerticalAlignment = VerticalAlignment.Top;
+
+                label.Name = "Character" + i.ToString();
+
+                label.Margin = new Thickness(i * label.Width, 0d, 0d, 0d);
+
+                GameGrid.Children.Add(label);
+            }
+        }
+
+        private void CreateCharacterBtns(char[] alph)
+        {
+            double bot = 0;
+            int count = 0;
+            for (int i = 0; i < alph.Length; i++, count++)
+            {
+                Button button = new Button();
+                button.FontSize = 20;
+                button.FontWeight = FontWeight;
+                button.HorizontalContentAlignment = HorizontalAlignment.Center;
+                button.VerticalContentAlignment = VerticalAlignment.Center;
+                button.Height = button.Width = 38;
+                button.HorizontalAlignment = HorizontalAlignment.Left;
+                button.VerticalAlignment = VerticalAlignment.Bottom;
+
+                button.Content = alph[i].ToString();
+
+                if ((count + 1) * button.Width > GameGrid.Width)
+                {
+                    count = 0;
+                    bot += button.Height;
+                }
+
+                button.Margin = new Thickness(count * button.Width, 0, 0, bot);
+                button.Click += new RoutedEventHandler(CharacterBtnClick);
+
+                GameGrid.Children.Add(button);
+            }
+        }
+        #endregion
     }
 }
